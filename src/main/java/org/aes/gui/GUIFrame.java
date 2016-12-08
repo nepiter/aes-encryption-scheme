b7cdf9e7f266dbc5cd09eb53931084aa61658cc7
yashwanth <yashwanth21093@gmail.com>
2016-12-08 16:38:13 -0600

    Update GUIFrame.java

--- src/main/java/org/aes/gui/GUIFrame.java
+++ src/main/java/org/aes/gui/GUIFrame.java
@@ -1,68 +1,71 @@
-package org.aes.gui;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
+import java.awt.event.ActionEvent;    
 import java.awt.event.ActionListener;
+import javax.swing.JButton;    
+import javax.swing.JFrame;    
+import javax.swing.JLabel;    
+import javax.swing.JPanel;    
+import javax.swing.JTextPane;
+import javax.swing.JComboBox;
+import java.awt.Font;
+import javax.swing.DefaultComboBoxModel;    
+public class MyFrame1 extends JFrame
+{
 
-public class GUIFrame extends JFrame {
-    private String plain = "";
-    private String temp;
-    private String temp1;
-    private String temp2;
-    private JComboBox EncryptionBitcount;
-    private JComboBox EncryptionType;
-    private JComboBox PaddingType;
+private int BitChoice=0;
+private int EncryptChoice=0; //0 for AES and 1 for RSA. 
+private int PaddingChoice = 0;
+private String InputString="";
+private String EncryptedString="";
+private String temp;
+private String temp1;
+private String temp2;
+private JComboBox EncryptionBitcount;
+private JComboBox EncryptionType;
+private JComboBox PaddingType; 
 
-    public GUIFrame() {
+    public MyFrame()
+    {
         super("Encrypt_Testing");
         setBounds(100, 100, 600, 900);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         getContentPane().setLayout(null);    
-     
         // PlainText Panel
+       
+        
         JPanel plainPanel = new JPanel();
         plainPanel.setBounds(0, 0, 584, 339);
         getContentPane().add(plainPanel);
         plainPanel.setLayout(null);                 
-        
         //PlainPane -> data setting -> final!
         final JTextPane PlainPane = new JTextPane();
         PlainPane.setBounds(12, 29, 560, 133);
         plainPanel.add(PlainPane);
-        
         // PlainText Label
         JLabel PlaintextLabel = new JLabel("PlainText");
         PlaintextLabel.setBounds(12, 10, 57, 15);
         plainPanel.add(PlaintextLabel);         
-        
         // CipherText Panel
         JPanel cipherPanel = new JPanel();
         cipherPanel.setBounds(0, 337, 584, 232);
         getContentPane().add(cipherPanel);
         cipherPanel.setLayout(null);    
-        
         // CipherText Label
         JLabel CipherLabel = new JLabel("CipherText");
         CipherLabel.setBounds(12, 10, 70, 15);
         cipherPanel.add(CipherLabel);           
-        
         //CipherPane -> data setting -> final!
         final JTextPane CipherPane = new JTextPane();
         CipherPane.setBounds(12, 36, 560, 133);
         cipherPanel.add(CipherPane);            
-        
         //Decipher Panel
         JPanel decipherPanel = new JPanel();
         decipherPanel.setBounds(0, 570, 584, 171);
         getContentPane().add(decipherPanel);
         decipherPanel.setLayout(null);                  
-        
         //Decipher Label
         JLabel DecipherLabel = new JLabel("DecipherText");
         DecipherLabel.setBounds(10, 1, 81, 24);
         decipherPanel.add(DecipherLabel);                   
-        
         //Decipher TextPane
         final JTextPane DecipherPane = new JTextPane();
         DecipherPane.setBounds(12, 24, 560, 133);
@@ -75,7 +78,8 @@ public class GUIFrame extends JFrame {
                 String cipher,result_de;
                 try{
                     cipher = CipherPane.getText();
-                   // result_de = FileEncryption.decryptString(cipher);
+                    ////////////////////////////////////////////////////////////////////////////////////////////
+                   // result_de = (cipher);
                     result_de = "yashwanth telekula";
                     DecipherPane.setText(result_de);
                 }catch (Exception e)
@@ -94,10 +98,11 @@ public class GUIFrame extends JFrame {
         {
         	String result_en;
             try {
-            	            	if(plain != "")
-                {plain = PlainPane.getText();}
+            	if(!InputString.equals(""))
+                InputString = PlainPane.getText();
+                
             	
-            	else {plain = PlainPane.getText();}
+                
             	/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
                 result_en = "It's working"; 
                 temp = (String) EncryptionBitcount.getSelectedItem();
@@ -108,9 +113,13 @@ public class GUIFrame extends JFrame {
                 temp = temp.substring(0,3);
                 temp1 = temp1.replaceAll(" ", "");
                 temp2 = temp2.replaceAll(" ", "");
-                System.out.println(temp);
-                System.out.println(temp1);
-                System.out.println(temp2);
+                
+                EncryptedString = encrypt(InputString, temp, temp1, temp2);
+                EncryptPane.setText(result_de);
+                //System.out.println(temp);
+                //System.out.println(temp1);
+                //System.out.println(temp2);
+                
                } catch (Exception e) {
                     // TODO Auto-generated catch block
                     e.printStackTrace();
@@ -125,12 +134,12 @@ public class GUIFrame extends JFrame {
         BrowseButton.addActionListener(new ActionListener() {
             public void actionPerformed(ActionEvent arg0) 
             {
-                String result_en;
+                
                 try {
                 	getFile a = new getFile();
                     
-                    String result_en = a.select();
-                	CipherPane.setText(result_en);
+                    InputString = a.select();
+                    //CipherPane.setText(text1);
                     } catch (Exception e) {
                         // TODO Auto-generated catch block
                         e.printStackTrace();
@@ -173,5 +182,10 @@ public class GUIFrame extends JFrame {
         plainPanel.add(PaddingType);
         PaddingType.setModel(new DefaultComboBoxModel(new String[] {"  EC13", "  CBC"}));
         PaddingType.setSelectedIndex(0);
+    }
+    
+    public static void main(String args[])
+    {
+        new MyFrame();
     }
 }
