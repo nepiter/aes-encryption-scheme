03a762494a94121009768b2716219e2b8f2e162e
Rushiraj Nenuji <rushirajnenuji@users.noreply.github.com>
2016-12-08 15:06:26 -0600

    Created getFile.java
    This file will select the file you want to select in the GUI. You can click on Browse and it will now work!!

---
+++ src/main/java/org/aes/gui/getFile.java
@@ -0,0 +1,47 @@
+/* Class for getting the file
+ * 
+ * Cryptography Project Fall 2016
+ * 
+ * This contains a method 'SELECT' that will be used to select the file that we will encrypted using both RAS 
+ * as well as AES.
+ *  
+ * */
+
+
+import javax.swing.JFileChooser;
+import javax.swing.LookAndFeel;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException; 
+
+public class getFile {
+	public String select() throws IOException, ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException{
+	    //JFileChooser fileChooser = new JFileChooser();
+	    
+	    JFileChooser fileChooser = null;
+	    LookAndFeel previousLF = UIManager.getLookAndFeel();
+	    UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+	    fileChooser = new JFileChooser();
+	    UIManager.setLookAndFeel(previousLF);
+	    String content = "";
+	    fileChooser.setCurrentDirectory(new File(System.getProperty("user.home")));
+	    int result = fileChooser.showOpenDialog(null);
+	    if (result == JFileChooser.APPROVE_OPTION) {
+	    	//scan.useDelimiter("\\Z");  ---- check the end of the file Sample.txt.txt 
+	        //content = new Scanner(fileChooser.getSelectedFile()).nextLine();   --- reads only one line but we want the entire content of the file. 
+	    	FileInputStream fis = new FileInputStream(fileChooser.getSelectedFile());
+	    	byte[] data = new byte[(int) fileChooser.getSelectedFile().length()];
+	    	fis.read(data);
+	    	fis.close();
+	    	content = new String(data, "UTF-8");
+	    }
+	    else{
+	    	System.out.println("File not choosen! You dumb ASS!");
+	    	content = null;
+	    }	    
+	    return content;
+	}	
+}
